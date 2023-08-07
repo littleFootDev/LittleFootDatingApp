893a0c0388da23a5372607331ac8ed8e3272f21a
LittleFoot <thelittlefootdev@gmail.com>
2023-08-07 07:23:26 +0200

    Create the skeleton of api

---
+++ LittleFootDatingApp-API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LittleFootDatingApp_API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+          _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers(){
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id){
+            return await  _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ LittleFootDatingApp-API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LittleFootDatingApp_API
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users {get; set;}
+    }
+}
